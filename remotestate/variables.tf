6acb26a03166402f9c7ba8cb759439e711355318
Kiran Baddi <baddy420@gmail.com>
2019-08-26 21:17:26 +0530

    multi-level build

---
+++ remotestate/variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "Prefix to all resources as part of common naming"
+  default     = "dev"
+}
+
+variable "region" {
+  description = "Prefix to all resources as part of common naming"
+  default     = "us-west-2"
+}
